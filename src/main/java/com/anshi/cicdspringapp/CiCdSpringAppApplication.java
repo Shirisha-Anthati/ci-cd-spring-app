1dfaff712017d72fcf1567c14d1a50053b7889ed
Anthati <2136945@cognizant.com>
2023-01-23 01:05:06 +0530

    Added project skeleton

---
+++ src/main/java/com/anshi/cicdspringapp/CiCdSpringAppApplication.java
@@ -0,0 +1,13 @@
+package com.anshi.cicdspringapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiCdSpringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiCdSpringAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anshi/cicdspringapp/CiCdSpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anshi.cicdspringapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiCdSpringAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
